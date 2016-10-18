5c738f1c2fda6533d412669dc528e6b1c3f563ac
van048 <yybbenyang@gmail.com>
2016-10-18 20:38:21 +0800

    Getting a Result from an Activity

--- app/src/main/java/cn/ben/googletraininginteractingwithapp/MainActivity.java
+++ app/src/main/java/cn/ben/googletraininginteractingwithapp/MainActivity.java
@@ -2,12 +2,15 @@ package cn.ben.googletraininginteractingwithapp;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.List;
@@ -33,6 +36,21 @@ public class MainActivity extends AppCompatActivity {
             showAppChooser();
         }
     };
+
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
+    public View.OnClickListener startActivityForResultOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            pickContact();
+        }
+    };
+
+    private void pickContact() {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        //The MIME type of CONTENT_URI providing a directory of phones.??
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
 
     private void createCalendarEvent() {
         // TODO: 2016/10/17 ACTION_INSERT?? CONTENT_URI???
@@ -119,6 +137,43 @@ public class MainActivity extends AppCompatActivity {
         // Verify the intent will resolve to at least one activity??
         if (emailIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(chooser);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+
+                // Do something with the contact here (bigger example below)
+                // Get the URI that points to the selected contact
+                Uri contactUri = data.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                // Perform the query on the contact to get the NUMBER column
+                // We don't need a selection or sort order (there's only one result for the given URI)
+                // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                // Consider using CursorLoader to perform the query.
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                if (cursor != null) {
+                    cursor.moveToFirst();
+
+                    // Retrieve the phone number from the NUMBER column
+                    int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    String number = cursor.getString(column);
+
+                    // Do something with the phone number...
+                    Toast.makeText(this, String.valueOf(number), Toast.LENGTH_SHORT).show();
+                    cursor.close();
+                }
+            }
         }
     }
 }
